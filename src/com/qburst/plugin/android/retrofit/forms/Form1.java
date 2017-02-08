0a504c33eff713b71e29745e0aba04e574f0a95e
ayshath <ayshath@qburst.com>
2017-02-08 17:11:58 +0530

    Filtering out module list

--- src/com/qburst/plugin/android/retrofit/forms/Form1.java
+++ src/com/qburst/plugin/android/retrofit/forms/Form1.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,6 +31,7 @@ public class Form1 {
     private List<Module> modules;
 
     private Form1() {
+        modules = new ArrayList<>();
         cancelButton.addActionListener(e -> controller.hideForm());
         //finishButton.addActionListener(e -> {});
         nextButton.addActionListener(e -> {
