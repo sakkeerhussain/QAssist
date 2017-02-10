a237bbad437164d2c223b13b0012095ab2e0e69a
Sakkeer Hussain <sakkeer@qburst.com>
2017-02-10 20:08:14 +0530

    Updated endhpoint url validation Reg exp

--- src/com/qburst/plugin/android/retrofit/Constants.java
+++ src/com/qburst/plugin/android/retrofit/Constants.java
@@ -63,7 +63,7 @@ public class Constants {
     }
 
     public class RegExp{
-        public static final String END_POINT_URL_PARAMS = "\\?(([a-zA-Z][a-zA-Z0-9_]*)=[^&=]+(&([a-zA-Z][a-zA-Z0-9_]*)=[^&=]+)*)?";
+        public static final String END_POINT_URL_PARAMS = "(\\?([a-zA-Z][a-zA-Z0-9_]*)=[^&=]+(&([a-zA-Z][a-zA-Z0-9_]*)=[^&=]+)*)?";
         public static final String END_POINT_URL_FIRST = "([a-zA-Z_/0-9]*((\\{{1})([a-zA-Z][a-zA-Z0-9_]*)=[^}]+(}{1})*)*)*";
         public static final String END_POINT_URL = END_POINT_URL_FIRST + END_POINT_URL_PARAMS;
     }
