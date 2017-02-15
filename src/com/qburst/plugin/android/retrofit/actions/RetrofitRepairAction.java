151fc601d72d4a38108bb81031a1059dd87918f1
Sakkeer Hussain <sakkeer@qburst.com>
2017-02-15 16:56:31 +0530

    Added progress bar while integrating retrofit

--- src/com/qburst/plugin/android/retrofit/actions/RetrofitRepairAction.java
+++ src/com/qburst/plugin/android/retrofit/actions/RetrofitRepairAction.java
@@ -2,10 +2,15 @@ package com.qburst.plugin.android.retrofit.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.JavaDirectoryService;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiPackage;
+import com.qburst.plugin.android.retrofit.Constants;
 import com.qburst.plugin.android.utils.notification.NotificationManager;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,14 +23,5 @@ public class RetrofitRepairAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         Project project = e.getProject();
         NotificationManager.get().showNotificationInfo(project, "Retrofit", "Repair", "Not yet implemented!");
-
-
-        ProgressManager.getInstance().run(new Task.Backgroundable(project, "Retrofit integrator") {
-            @Override
-            public void run(@NotNull ProgressIndicator indicator) {
-                indicator.setIndeterminate(false);
-                indicator.setFraction(5);
-            }
-        });
     }
 }
