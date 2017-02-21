b94cd674b456d5726d863edcf9d7092aaf712b2d
ayshath <ayshath@qburst.com>
2017-02-21 15:38:56 +0530

    intermediate-repair

--- src/com/qburst/plugin/android/retrofit/actions/RetrofitRepairAction.java
+++ src/com/qburst/plugin/android/retrofit/actions/RetrofitRepairAction.java
@@ -1,5 +1,6 @@
 package com.qburst.plugin.android.retrofit.actions;
 
+import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.command.WriteCommandAction;
@@ -7,10 +8,11 @@ import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.JavaDirectoryService;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiPackage;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.source.PsiJavaFileImpl;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.qburst.plugin.android.retrofit.Constants;
+import com.qburst.plugin.android.utils.log.Log;
 import com.qburst.plugin.android.utils.notification.NotificationManager;
 import org.jetbrains.annotations.NotNull;
 
@@ -23,12 +25,22 @@ public class RetrofitRepairAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         Project project = e.getProject();
         NotificationManager.get().showNotificationInfo(project, "Retrofit", "Repair", "Not yet implemented!");
+
     }
 
     @Override
     public void update(AnActionEvent e) {
         super.update(e);
-            e.getPresentation().setEnabled(false);
+        PsiClass managerClass = JavaPsiFacade.getInstance(e.getProject()).findClass("com.qburst.retrofit.RetrofitManager",GlobalSearchScope.allScope(e.getProject()));
+        if (managerClass == null){
+            Log.d("not-found", "manger class not found");
+        }else{
+            Log.d("found", managerClass.getQualifiedName());
+        }
+        e.getPresentation().setEnabled(false);
 
     }
+
+
 }
+
