1e62a069c9bef56397c4ed3459579675e54d5342
Sakkeer Hussain <sakkeer@qburst.com>
2017-04-11 16:15:57 +0530

    Removed second source root

--- src/com/qassist/plugin/android/utils/classutils/ParameterModel.java
+++ src/com/qassist/plugin/android/utils/classutils/ParameterModel.java
@@ -1,12 +1,5 @@
 package com.qassist.plugin.android.utils.classutils;
 
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementFactory;
-import com.intellij.psi.PsiField;
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONObject;
-
 /**
  * Created by sakkeer on 28/2/17.
  */
